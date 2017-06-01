32a2fef221f5a27b5bf2e5679261a82b5b193194
NevilDabre <nevil_dabre@yahoo.com>
2017-06-01 00:48:50 -0400

    Slider and timer implemented

--- Assignment1/ViewController.h
+++ Assignment1/ViewController.h
@@ -10,10 +10,18 @@
 
 @interface ViewController : UIViewController
 
+//created a slider pointer
 @property (weak, nonatomic) IBOutlet UISlider *UlSliderSalary;
 
+//created a label pointer to display selected variable
 @property (weak, nonatomic) IBOutlet UILabel *UILabelCurrentSalary;
 
+//created a label pointer to display 
+@property (weak, nonatomic) IBOutlet UILabel *m_currentSalary;
+
+@property double m_cost;
+@property double m_user_salary;
+@property double m_salary_per_second;
 
 @end
 
--- Assignment1/ViewController.m
+++ Assignment1/ViewController.m
@@ -14,22 +14,47 @@
 
 @implementation ViewController
 
+NSTimer *t;
+//Total number of seconds 7488000
+
+-(void)onTick:(NSTimer *)timer{
+    
+    _m_cost += (_m_user_salary/7488000);
+    _m_currentSalary.text = [NSString stringWithFormat:@"$%f",_m_cost];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    
+    _m_cost = 0.00;
 }
 
 - (IBAction)UISliderSalaryChange:(id)sender {
     
-    //self.UILabelCurrentSalary.text = [NSString stringWithFormat:@"%f", self.UlSliderSalary.value];
+    //read value of a slider
+    _m_user_salary = self.UlSliderSalary.value;
     
-    float number = self.UlSliderSalary.value;
-    int value = (int)(number * 1);
+    //convert slider value into int
+    _m_user_salary = (int)(_m_user_salary * 1);
     
-    self.UILabelCurrentSalary.text = [NSString stringWithFormat:@"%i", value];
+    //display slider value to user
+    self.UILabelCurrentSalary.text = [NSString stringWithFormat:@"$%.f", _m_user_salary];
 }
+- (IBAction)startTimer:(id)sender {
+    
+
+    t = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector: @selector(onTick:) userInfo:nil repeats:YES];
+    
+}
+
+- (IBAction)stopTimer:(id)sender {
+    if(t){
+        [t invalidate];
+    }
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
